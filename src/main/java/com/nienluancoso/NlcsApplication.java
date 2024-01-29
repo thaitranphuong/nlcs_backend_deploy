138c1f79fa604162c577a940c70b6bb79a0d5839
Trần Phương Thái <140373113+thaitranphuong@users.noreply.github.com>
2024-01-29 12:48:53 +0000

    add timmer

--- src/main/java/com/nienluancoso/NlcsApplication.java
+++ src/main/java/com/nienluancoso/NlcsApplication.java
@@ -1,5 +1,8 @@
 package com.nienluancoso;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +11,14 @@ public class NlcsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NlcsApplication.class, args);
+		Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new PrintHelloTask(), 0, 10000); // Schedule the task to run every 5 seconds
 	}
 
+	static class PrintHelloTask extends TimerTask {
+        @Override
+        public void run() {
+            System.out.println("hello");
+        }
+    }
 }
